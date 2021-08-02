a803db156cf87903a03745ba7ca544b39d2f9b23
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-08-02 18:12:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-function preload() {
-	world_start = loadSound("world_start.wav");
-	setSprites();
-	MarioAnimation();
-}
+img = "";
+noseX = 0;
+noseY = 0;
+
+
+
 
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
-	instializeInSetup(mario);
+  video = createCapture(VIDEO);
+  video.size(600,300);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+  if(results.length>0){
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX:" + noseX + ", noseY:" + noseY);
+  }
+}
+
+function modelLoaded(){
+  console.log("Model is Loaded !")
 }
 
 function draw() {
-	game()
+  background("lightgreen");
+  
+  
 }
+
+
+
+
+
+
+
+
+
 
 
 
